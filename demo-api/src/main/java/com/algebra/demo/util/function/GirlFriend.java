42cdfad09c2918b82489ea1b536e078d37c880dd
lilong <1136125618@qq.com>
2020-02-02 14:28:35 +0800

    新增util

---
+++ demo-api/src/main/java/com/algebra/demo/util/function/GirlFriend.java
@@ -0,0 +1,90 @@
+package com.algebra.demo.util.function;
+
+import com.algebra.demo.util.base.Builder;
+import com.google.common.base.Function;
+import com.google.common.base.Supplier;
+import lombok.Data;
+
+import java.util.*;
+
+/**
+ * @author al
+ * @date 2020/1/16 15:38
+ * @description
+ */
+@Data
+public class GirlFriend {
+
+    private String name;
+    private int age;
+    private int bust;
+    private int waist;
+    private int hips;
+    private List<String> hobby;
+    private String birthday;
+    private String address;
+    private String mobile;
+    private String email;
+    private String hairColor;
+    private Map<String, String> gift;
+
+    // 为了演示方便，加几个聚合方法
+    public void addHobby(String hobby) {
+        this.hobby = Optional.ofNullable(this.hobby).orElse(new ArrayList<>());
+        this.hobby.add(hobby);
+    }
+    public void addGift(String day, String gift) {
+        this.gift = Optional.ofNullable(this.gift).orElse(new HashMap<>());
+        this.gift.put(day, gift);
+    }
+    public void setVitalStatistics(int bust, int waist, int hips) {
+        this.bust = bust;
+        this.waist = waist;
+        this.hips = hips;
+    }
+
+    public static int compareGirlFriendByAge(GirlFriend g1, GirlFriend g2){
+        return g1.getAge() - g2.getAge();
+    }
+
+    public static void main(String[] args) {
+        GirlFriend myGirlFriend = Builder.of(GirlFriend::new)
+                .with(GirlFriend::setName, "小美")
+                .with(GirlFriend::setAge, 18)
+                .with(GirlFriend::setVitalStatistics, 33, 23, 33)
+                .with(GirlFriend::setBirthday, "2001-10-26")
+                .with(GirlFriend::setAddress, "上海浦东")
+                .with(GirlFriend::setMobile, "18688888888")
+                .with(GirlFriend::setEmail, "pretty-xiaomei@qq.com")
+                .with(GirlFriend::setHairColor, "浅棕色带点微卷")
+                .with(GirlFriend::addHobby, "逛街")
+                .with(GirlFriend::addHobby, "购物")
+                .with(GirlFriend::addHobby, "买东西")
+                .with(GirlFriend::addGift, "情人节礼物", "LBR 1912女王时代")
+                .with(GirlFriend::addGift, "生日礼物", "迪奥烈焰蓝金")
+                .with(GirlFriend::addGift, "纪念日礼物", "阿玛尼红管唇釉")
+                // 等等等等 ...
+                .build();
+
+        System.out.println(myGirlFriend.toString());
+
+        List<GirlFriend> girlFriends = new ArrayList<>();
+        girlFriends.add(myGirlFriend);
+
+        // 四种方法引用及其返回值
+        // 1.对象::实例方法名
+        Supplier<String> getAddress = myGirlFriend::getAddress;
+        // 2.类名::实例方法名
+        Function<GirlFriend, String> getAddress1 = GirlFriend::getAddress;
+        // 3.类名::静态方法名
+        Comparator<GirlFriend> compareGirlFriendByAge = GirlFriend::compareGirlFriendByAge;
+
+        girlFriends.sort((o1, o2) -> GirlFriend.compareGirlFriendByAge(o1,o2));
+        girlFriends.sort(GirlFriend::compareGirlFriendByAge);
+        girlFriends.forEach(s -> System.out.println(s.getAge()));
+        // 4.类名::new
+        Supplier<GirlFriend> girlFriendSupplier = GirlFriend::new;
+
+    }
+
+}
---
+++ demo-api/src/main/java/com/algebra/demo/util/function/SupplierTest.java
@@ -0,0 +1,87 @@
+package com.algebra.demo.util.function;
+
+
+import java.util.Optional;
+import java.util.Random;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+/**
+ * @author al
+ * @date 2020/1/19 17:21
+ * @description
+ */
+public class SupplierTest {
+
+    public void baseTest(){
+        // 使用Supplier接口实现方法,只有一个get方法，无参数，返回一个值
+        Supplier<Integer> supplier = new Supplier<Integer>() {
+            @Override
+            public Integer get() {
+                return new Random().nextInt();
+            }
+        };
+
+        // 2.lambda表达式
+        Supplier<Integer> supplier2 = () -> new Random().nextInt();
+
+        // 3.方法引用
+        Supplier<Double> supplier3 =  Math::random;
+
+        System.out.println(supplier3.get());
+
+    }
+
+    public void test2(){
+        Stream<Integer> stream = Stream.of(1,2,3,4,5);
+
+        // 返回一个optional对象
+        Optional<Integer> first = stream.filter(i -> i > 4).findFirst();
+
+        //optional对象有需要Supplier接口的方法
+        //orElse，如果first中存在数，就返回这个数，如果不存在，就放回传入的数
+        System.out.println(first.orElse(1));
+        System.out.println(first.orElse(7));
+
+        System.out.println("---------------------------");
+
+        Supplier<Integer> supplier = () -> {
+            //返回一个随机值
+            return new Random().nextInt();
+        };
+
+        //orElseGet，如果first中存在数，就返回这个数，如果不存在，就返回supplier返回的值
+        System.out.println(first.orElseGet(supplier));
+
+
+    }
+
+    public void test3() {
+
+        // 1.简单模式
+        Supplier<GirlFriend> supplier = new Supplier<GirlFriend>() {
+            @Override
+            public GirlFriend get() {
+                return new GirlFriend();
+            }
+        };
+
+        // 2.lambda表达式
+        Supplier<GirlFriend> supplier2 = () -> new GirlFriend();
+
+        // 3.引用
+        Supplier<GirlFriend> supplier3 = GirlFriend::new;
+
+    }
+
+
+
+    public static void main(String[] args) {
+
+        SupplierTest test = new SupplierTest();
+        test.test2();
+
+
+    }
+
+}
