4a8ce0ff4c5d0d55909f19bdb5c75a9f1b8891dc
lilong <lilong@zhongruigroup.com>
2020-02-14 13:30:51 +0800

    分库分表 shard 1.0

--- demo-shard/src/main/java/com/algebra/demofastdep/web/LessonController.java
+++ demo-shard/src/main/java/com/algebra/demofastdep/web/LessonController.java
@@ -2,7 +2,7 @@ package com.algebra.demofastdep.web;
 
 import com.algebra.demo.util.base.WebApiResult;
 import com.algebra.demofastdep.entity.domain.Lesson;
-import com.algebra.demofastdep.service.wrapper.LessonServiceWrapper;
+import com.algebra.demofastdep.service.LessonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class LessonController {
 
     @Autowired
-    LessonServiceWrapper lessonService;
+    LessonService lessonService;
 
     @GetMapping("/getLessonInfo")
     public WebApiResult<Lesson> getLessonInfo(@RequestParam("id") String lessonId){
