a3d7d50e7b32ca8d9338cb0e72e92da1a73d28ac
lilong <lilong@zhongruigroup.com>
2020-03-09 17:56:01 +0800

    StreamMq partition

---
+++ demo-api/src/main/java/com/algebra/demo/util/nio/NioTest01.java
@@ -0,0 +1,19 @@
+package com.algebra.demo.util.nio;
+
+/**
+ * @author al
+ * @date 2020/3/9 16:18
+ * @description
+ */
+public class NioTest01 {
+
+    public static void largeFileIo(String inputFile, String outPutFile){
+
+    }
+
+    public static void readLargeTextWithNio(){
+
+    }
+
+
+}
