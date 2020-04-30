55fe3540acfe38c9d75e164eaf8aa70956e44b94
lilong <lilong@zhongruigroup.com>
2020-04-30 14:02:43 +0800

    NIO dataway

---
+++ dataway/src/main/java/com/algebra/dataway/demo/conf/ExampleModule.java
@@ -0,0 +1,30 @@
+package com.algebra.dataway.demo.conf;
+
+import net.hasor.core.ApiBinder;
+import net.hasor.core.DimModule;
+import net.hasor.db.JdbcModule;
+import net.hasor.db.Level;
+import net.hasor.spring.SpringModule;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+
+/**
+ * @author al
+ * @date 2020/4/30 13:24
+ * @description
+ */
+@DimModule
+@Component
+public class ExampleModule implements SpringModule {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    public void loadModule(ApiBinder apiBinder) throws Throwable {
+        // .DataSource form Spring boot into Hasor
+        apiBinder.installModule(new JdbcModule(Level.Full, this.dataSource));
+    }
+}
