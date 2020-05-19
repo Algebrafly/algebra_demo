2778ff080610438ad539103166f997b79037017d
ZR19040088-李龙 <lilong@zhongruigroup.com>
2020-05-19 18:24:26 +0800

    算法重构

--- demo-api/src/main/java/com/algebra/demo/study/PrimesGenerator.java
+++ demo-api/src/main/java/com/algebra/demo/study/PrimesGenerator.java
@@ -5,76 +5,72 @@ import java.util.Arrays;
 /**
  * @author al
  * @date 2020/5/19 15:53
- * @description 筛选法求N以内的所以素数
- * @verson 2.0
+ * @description 筛选法求N以内的所以素数 --> 拆解方法 & 改方法名和缩减变量
+ * @verson 2.0 --> 2.1
  */
 public class PrimesGenerator {
 
-    private static boolean[] f;
-    private static int[] primes;
-    private static int s;
+    private static boolean[] isCrossed;
+    private static int[] result;
 
-    public static int[] generatorPrimes(int maxValue){
-        if(maxValue < 2){
+    public static int[] generatorPrimes(int maxValue) {
+        if (maxValue < 2) {
             return new int[0];
         } else {
             // 初始化数据
-            initializeSieve(maxValue);
+            initializeArrayOfInteger(maxValue);
             // 筛选法
-            sieve();
+            crossOutMultiples();
             // 加载数据
-            loadPrimes();
-            return primes;
+            putUncrossIntegerIntoResult();
+            return result;
         }
     }
 
-    public static void initializeSieve(int maxValue){
-        s = maxValue + 1;
-        f = new boolean[s];
+    public static void initializeArrayOfInteger(int maxValue) {
+        isCrossed = new boolean[maxValue + 1];
         int i;
-
+        isCrossed[0] = isCrossed[1] = false;
         // initialize array to true
-        for (i = 0; i < s; i++) {
-            f[i] = true;
+        for (i = 2; i < isCrossed.length; i++) {
+            isCrossed[i] = true;
         }
 
-        f[0] = f[1] = false;
     }
 
-    public static void sieve(){
+    public static void crossOutMultiples() {
         // sieve (过滤)
         int i;
         int j;
-        for (i = 2; i < Math.sqrt(s) + 1; i++) {
+        for (i = 2; i < Math.sqrt(isCrossed.length) + 1; i++) {
             // 使用筛法求N以内的素数，从2开始，不断剔除2的倍数，然后从剩下的数字中，选择最小的数3（这个数一定会是素数），然后剔除所有3的倍数，
             // 依次类推，最后剩下的数就全是素数了。
-            for (j = 2 * i; j < s; j += i) {
-                f[j] = false;
+            for (j = 2 * i; j < isCrossed.length; j += i) {
+                isCrossed[j] = false;
             }
         }
     }
 
 
-    private static void loadPrimes(){
+    private static void putUncrossIntegerIntoResult() {
         int i;
         int j;
 
         // how many primes are there?
         int count = 0;
-        for (i = 0; i < s; i++) {
-            if (f[i]) {
+        for (i = 0; i < isCrossed.length; i++) {
+            if (isCrossed[i]) {
                 count++;
             }
         }
 
         // move the primes into the result
-        primes = new int[count];
-        for (i = 0, j = 0; i < s; i++) {
-            if (f[i]) {
-                primes[j++] = i;
+        result = new int[count];
+        for (i = 0, j = 0; i < isCrossed.length; i++) {
+            if (isCrossed[i]) {
+                result[j++] = i;
             }
         }
-
     }
 
 
@@ -93,7 +89,6 @@ public class PrimesGenerator {
 
         int[] centArray = generatorPrimes(100);
         System.out.println(Arrays.toString(centArray));
-
 
 
     }
