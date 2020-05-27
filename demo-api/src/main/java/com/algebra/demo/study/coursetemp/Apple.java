f371c1dbebda472f2b83975ca54a6da348d2d749
ZR19040088-李龙 <lilong@zhongruigroup.com>
2020-05-27 17:00:41 +0800

    课程教学

--- demo-api/src/main/java/com/algebra/demo/study/coursetemp/Apple.java
+++ demo-api/src/main/java/com/algebra/demo/study/coursetemp/Apple.java
@@ -1,6 +1,8 @@
 package com.algebra.demo.study.coursetemp;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
@@ -10,6 +12,8 @@ import java.util.Date;
  * @description
  */
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Apple extends Fruit {
 
     private String address;
--- demo-api/src/main/java/com/algebra/demo/study/coursetemp/Test10.java
+++ demo-api/src/main/java/com/algebra/demo/study/coursetemp/Test10.java
@@ -1,6 +1,7 @@
 package com.algebra.demo.study.coursetemp;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -16,11 +17,17 @@ public class Test10 {
 //        Plate<Fruit> p = new Plate<Apple>(new Apple());
 
         // 一个能放水果以及一切是水果派生类的盘子。ps.啥水果都能放的盘子。
-        Plate<? extends Fruit> p = new Plate<Apple>(new Apple());
+        Plate<? extends Fruit> p = new Plate<Apple>(new Apple("aa",new Date()));
+        Fruit item = p.getItem();
+//        p.setItem(new Fruit());
+        System.out.println(item);
 
 
         // 一个能放水果以及一切是水果基类的盘子。
-        Plate<? super Fruit> p2 = new Plate<>(new Apple());
+        Plate<? super Fruit> p2 = new Plate<>(new Apple("bb",new Date()));
+        Object item1 = p2.getItem();
+        System.out.println(item1);
+        p2.setItem(new Apple("cc",new Date()));
 
     }
 
@@ -42,7 +49,7 @@ public class Test10 {
 
 
     public static void main(String[] args)  {
-
+        testPlate();
 
 
 
