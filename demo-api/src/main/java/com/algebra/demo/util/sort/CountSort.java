726c23df47438ec8243057cd0589303382cc067c
ZR19040088-李龙 <lilong@zhongruigroup.com>
2020-11-03 11:26:10 +0800

    计数排序 v1

--- demo-api/src/main/java/com/algebra/demo/util/sort/CountSort.java
+++ demo-api/src/main/java/com/algebra/demo/util/sort/CountSort.java
@@ -1,5 +1,7 @@
 package com.algebra.demo.util.sort;
 
+import java.util.Arrays;
+
 /**
  * @author al
  * @date 2020/11/3 11:06
@@ -7,7 +9,41 @@ package com.algebra.demo.util.sort;
  */
 public class CountSort {
 
+    public static int[] countSortV1(int[] array) {
+        // 1.得到数列最大值
+        int max = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
 
+        // 2.根据数列最大值确定统计数组长度
+        int[] countArray = new int[max + 1];
+
+        // 3.遍历数列，填充统计数组
+        for (int i = 0; i < array.length; i++) {
+            countArray[array[i]]++;
+        }
+
+        // 4.遍历统计数组
+        int index = 0;
+        int[] sortedArray = new int[array.length];
+        for (int i = 0; i < countArray.length; i++) {
+            for (int j = 0; j < countArray[i]; j++) {
+                sortedArray[index++] = i;
+            }
+        }
+
+        return sortedArray;
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[]{4, 4, 6, 5, 3, 2, 8, 1, 7, 5, 6, 0, 10};
+        int[] sortedArray = countSortV1(array);
+        System.out.println(Arrays.toString(sortedArray));
+
+    }
 
 
 }
