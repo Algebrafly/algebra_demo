51a492e424ba8082ce50256c3eee3f3a5dae3632
ZR19040088-李龙 <lilong@zhongruigroup.com>
2020-11-04 15:01:46 +0800

    排序算法补充

--- demo-api/src/main/java/com/algebra/demo/util/sort/MergeSort.java
+++ demo-api/src/main/java/com/algebra/demo/util/sort/MergeSort.java
@@ -39,6 +39,7 @@ public class MergeSort {
         int j = middle+1;
         int n = end;
         int k = 0;
+        // 比较左右两部分的元素，哪个小，把那个元素填入temp中
         while(i<=m && j<=n){
             if(a[i] <= a[j]){
                 temp[k] = a[i];
@@ -50,6 +51,8 @@ public class MergeSort {
                 j++;
             }
         }
+
+        // 上面的循环退出后，把剩余的元素依次填入到temp中
         while(i<=m){
             temp[k] = a[i];
             k++;
@@ -61,8 +64,33 @@ public class MergeSort {
             j++;
         }
 
+        // 把最终的排序的结果复制给原数组
         for(int ii=0;ii<k;ii++){
             a[first + ii] = temp[ii];
+        }
+    }
+
+    // 改进版-代码简化版
+    public static void merge(int[] arr, int L, int mid, int R) {
+        int[] temp = new int[R - L + 1];
+        int i = 0;
+        int p1 = L;
+        int p2 = mid + 1;
+        // 比较左右两部分的元素，哪个小，把那个元素填入temp中
+        while(p1 <= mid && p2 <= R) {
+            temp[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
+        }
+        // 上面的循环退出后，把剩余的元素依次填入到temp中
+        // 以下两个while只有一个会执行
+        while(p1 <= mid) {
+            temp[i++] = arr[p1++];
+        }
+        while(p2 <= R) {
+            temp[i++] = arr[p2++];
+        }
+        // 把最终的排序的结果复制给原数组
+        for(i = 0; i < temp.length; i++) {
+            arr[L + i] = temp[i];
         }
     }
 
