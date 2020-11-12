bd6d175834bc0587b836fe60f1501aaeaecad930
ZR19040088-李龙 <lilong@zhongruigroup.com>
2020-11-12 17:01:29 +0800

    背包问题-动态规划

--- demo-api/src/main/java/com/algebra/demo/util/interview/DynamicProgramming.java
+++ demo-api/src/main/java/com/algebra/demo/util/interview/DynamicProgramming.java
@@ -8,12 +8,12 @@ import java.util.Arrays;
  * @description 【小灰的算法之旅-java版实现】- 动态规划问题：1.金矿 2.背包
  * w个工人，开采n个黄金矿，每个矿的黄金含量不一样，需要的人力也不一样，怎样分配人力开采能获取最大黄金量？
  * 将复杂问题转化成子问题：1.确定问题边界；2.确定状态转移方程
- *
+ * <p>
  * 参考（动态规划经典问题）：
- *      https://blog.csdn.net/program_developer/article/details/85274825
- *      https://www.cnblogs.com/cruelty_angel/p/10576617.html
- *      https://www.jianshu.com/p/866c0dd53619
- *
+ * https://blog.csdn.net/program_developer/article/details/85274825
+ * https://www.cnblogs.com/cruelty_angel/p/10576617.html
+ * https://www.jianshu.com/p/866c0dd53619
+ * https://www.cnblogs.com/bokeyuan-dlam/articles/9453128.html
  */
 public class DynamicProgramming {
 
@@ -70,6 +70,82 @@ public class DynamicProgramming {
     }
 
     /**
+     * 背包问题
+     * 时间复杂度和空间复杂度：O(capacity*n)
+     * 数学模型：
+     * 1）MaxValue(i, j) = MaxValue(i-1, j) , j < size(i-1)
+     * 2) MaxValue(i, j) = Max{MaxValue(i-1, j), MaxValue(i-1, j-size[i-1]) + values[i-1]} , j >= size(i-1)
+     *
+     * @param capacity 背包容量
+     * @param values   每件物品的价值数组
+     * @param sizes    每件物品的体积数组
+     * @return 背包所能容纳物品的最大价值
+     */
+    public static int getBestPackageValueV1(int capacity, int[] values, int[] sizes) {
+        int[][] resultTable = new int[values.length + 1][capacity + 1];
+        int[][] path = new int[values.length + 1][capacity + 1];
+        for (int i = 1; i <= values.length; i++) {
+            for (int j = 1; j <= capacity; j++) {
+                if (j < sizes[i - 1]) {
+                    // 如果当前背包容量 < 当前物品体积
+                    resultTable[i][j] = resultTable[i - 1][j];
+                } else {
+                    resultTable[i][j] = Math.max(resultTable[i - 1][j], resultTable[i - 1][j - sizes[i - 1]] + values[i - 1]);
+                    // 标记商品放入背包
+                    path[i][j] = 1;
+                }
+            }
+        }
+        System.out.println(Arrays.deepToString(resultTable));
+        boolean[] item = new boolean[values.length];
+        getBestPackageWhat(values.length, capacity, values, sizes, resultTable, item);
+        System.out.println("最优组合解：" + Arrays.toString(item));
+
+        getBestPackageWhatV2(path,sizes);
+
+        return resultTable[values.length][capacity];
+    }
+
+    /**
+     * 寻找最优组合解
+     * 1) V(i,j)=V(i-1,j)时，说明没有选择第i个商品，则回到V(i-1,j)；
+     * 2) V(i,j)=V(i-1,j-w(i-1))+v(i-1)时，说明装了第i个商品，该商品是最优解组成的一部分，随后我们得回到装该商品之前，即回到V(i-1,j-w(i-1))；
+     * 3) 一直遍历到i＝0结束为止，所有解的组成都会找到。
+     *
+     * @param i           values.length
+     * @param j           capacity
+     * @param values      values
+     * @param sizes       sizes
+     * @param resultTable 结果表
+     * @param item        商品标记数组
+     */
+    public static void getBestPackageWhat(int i, int j, int[] values, int[] sizes, int[][] resultTable, boolean[] item) {
+        if (i > 0) {
+            if (resultTable[i][j] == resultTable[i - 1][j]) {
+                item[i - 1] = false;
+                getBestPackageWhat(i - 1, j, values, sizes, resultTable, item);
+            } else if ((j - sizes[i - 1] >= 0) && (resultTable[i][j] == (resultTable[i - 1][j - sizes[i - 1]] + values[i - 1]))) {
+                item[i - 1] = true;
+                getBestPackageWhat(i - 1, j - sizes[i - 1], values, sizes, resultTable, item);
+            }
+        }
+    }
+
+    public static void getBestPackageWhatV2(int[][] path, int[] sizes){
+        int i = path.length-1;
+        int j = path[0].length-1;
+        while (i > 0 && j > 0) {
+            if(path[i][j] == 1){
+                System.out.printf("第%d个商品放入背包；\n",i);
+                j -= sizes[i-1];
+            }
+            i--;
+        }
+
+    }
+
+
+    /**
      * 自底向上求解(优化空间结构)
      * 时间复杂度：O(nw)
      * 空间复杂度：O(n)
@@ -82,13 +158,13 @@ public class DynamicProgramming {
     public static int getBestGoldMiningV3(int w, int[] p, int[] g) {
 
         // 当前结果集
-        int[] results = new int[w+1];
+        int[] results = new int[w + 1];
 
         // 填充一维数组
         for (int i = 1; i <= g.length; i++) {
             for (int j = w; j >= 1; j--) {
-                if(j >= p[i-1]){
-                    results[j] = Math.max(results[j], results[j-p[i-1]] + g[i-1]);
+                if (j >= p[i - 1]) {
+                    results[j] = Math.max(results[j], results[j - p[i - 1]] + g[i - 1]);
                 }
             }
         }
@@ -99,11 +175,19 @@ public class DynamicProgramming {
 
 
     public static void main(String[] args) {
+
+        System.out.println("金矿问题Test");
         int w = 10;
         int[] p = {5, 5, 3, 4, 3};
         int[] g = {400, 500, 200, 300, 350};
 //        System.out.println("最优收益：" + getBestGoldMiningV2(w, p, g));
         System.out.println("最优收益：" + getBestGoldMiningV3(w, p, g));
+
+        System.out.println("背包问题Test");
+        int capacity = 8;
+        int[] values = {3, 4, 5, 6};
+        int[] sizes = {2, 3, 4, 5};
+        System.out.println("最优收益：" + getBestPackageValueV1(capacity, values, sizes));
 
     }
 
