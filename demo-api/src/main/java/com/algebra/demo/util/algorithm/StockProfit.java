102bdf7e4baa5cd598f6054ecf8e5c442c7c2d17
ZR19040088-李龙 <lilong@zhongruigroup.com>
2020-11-13 17:07:35 +0800

    红包算法：二倍均值法、线段切割法

--- demo-api/src/main/java/com/algebra/demo/util/algorithm/StockProfit.java
+++ demo-api/src/main/java/com/algebra/demo/util/algorithm/StockProfit.java
@@ -27,8 +27,9 @@ public class StockProfit {
     public int maxProfitForAnyTime(int[] prices) {
         int maxProfit = 0;
         for (int i = 1; i < prices.length; i++) {
-            if (prices[i] > prices[i-1])
+            if (prices[i] > prices[i-1]) {
                 maxProfit += prices[i] - prices[i-1];
+            }
         }
         return maxProfit;
     }
