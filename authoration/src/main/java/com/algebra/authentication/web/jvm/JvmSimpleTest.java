42811fd92a2bb3bd690999294d76639fab864580
ZR19040088-李龙 <lilong@zhongruigroup.com>
2021-07-23 09:52:48 +0800

    测试接口超时

--- authoration/src/main/java/com/algebra/authentication/web/jvm/JvmSimpleTest.java
+++ authoration/src/main/java/com/algebra/authentication/web/jvm/JvmSimpleTest.java
@@ -1,9 +1,14 @@
 package com.algebra.authentication.web.jvm;
 
+import cn.hutool.core.date.DateField;
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.date.DateUtil;
 import com.algebra.authentication.domain.SysUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 /**
  * @author al
@@ -19,6 +24,24 @@ public class JvmSimpleTest {
 //            new SysUser();
 //        }
         return "hello "+name;
+    }
+
+    public static void main(String[] args) {
+
+//        Date cuur = new Date();
+
+        DateTime curr = DateUtil.date();
+        curr.setField(DateField.HOUR_OF_DAY,0);
+        curr.setField(DateField.MINUTE,0);
+        curr.setField(DateField.SECOND,0);
+
+        System.out.println(curr.toString());
+
+        DateTime offset = DateUtil.offset(curr, DateField.DAY_OF_MONTH, -7);
+
+        System.out.println(offset.toString());
+        System.out.println(offset.toDateStr());
+
     }
 
 }
