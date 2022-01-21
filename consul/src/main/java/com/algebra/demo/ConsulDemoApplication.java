8b3124d217d00183a21e46bd4f9cabdd2d5decb6
ZR19040088-lilong <lilong@zhongruigroup.com>
2022-01-21 15:22:45 +0800

    gateway

--- consul/src/main/java/com/algebra/demo/ConsulDemoApplication.java
+++ consul/src/main/java/com/algebra/demo/ConsulDemoApplication.java
@@ -3,11 +3,16 @@ package com.algebra.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableConfigurationProperties
+@EnableHystrix
+@EnableHystrixDashboard
 public class ConsulDemoApplication {
 
     public static void main(String[] args) {
