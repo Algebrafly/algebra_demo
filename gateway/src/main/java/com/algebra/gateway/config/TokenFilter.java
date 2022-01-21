401fe0c49dbfd9fafd39bcc5ec270db0118ccf9a
ZR19040088-lilong <lilong@zhongruigroup.com>
2022-01-21 16:31:26 +0800

    gateway

---
+++ gateway/src/main/java/com/algebra/gateway/config/TokenFilter.java
@@ -0,0 +1,26 @@
+package com.algebra.gateway.config;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author al
+ * @date 2022/1/21 15:24
+ * @description
+ */
+public class TokenFilter implements GlobalFilter, Ordered {
+
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        return null;
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
