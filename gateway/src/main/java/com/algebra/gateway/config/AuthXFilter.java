883411266d0037085e9914460b6f5a5397901418
ZR19040088-lilong <lilong@zhongruigroup.com>
2022-01-24 17:56:19 +0800

    Authorization

--- gateway/src/main/java/com/algebra/gateway/config/AuthXFilter.java
+++ gateway/src/main/java/com/algebra/gateway/config/AuthXFilter.java
@@ -9,14 +9,19 @@ import reactor.core.publisher.Mono;
 /**
  * @author al
  * @date 2022/1/21 15:24
- * @description
+ * @description Authentication【认证】、Authorization【授权】
+ *
  */
-public class TokenFilter implements GlobalFilter, Ordered {
+public class AuthXFilter implements GlobalFilter, Ordered {
 
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        return null;
+
+
+
+
+        return chain.filter(exchange);
     }
 
     @Override
